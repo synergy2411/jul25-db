45ac071ed114704c44dc16b6877d41bf838fac41
Sumit Khandelwal <synergy2411@outlook.com>
2025-07-30 15:28:27 +0530

    JDBC User details manager added

--- day04/accounts/src/main/java/com/eazybank/accounts/configuration/SecurityConfiguration.java
+++ day04/accounts/src/main/java/com/eazybank/accounts/configuration/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.eazybank.accounts.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -10,13 +11,21 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfiguration {
+
+    @Autowired
+    DataSource dataSource;          // h2 Database
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -35,12 +44,30 @@ public class SecurityConfiguration {
         return httpSecurity.build();
     }
 
+//    @Bean
+//    public UserDetailsService userDetailsService(){
+//        UserDetails monica = User.withUsername("monica").password("{noop}monica123").roles("MANAGER").build();
+//        UserDetails joey = User.withUsername("joey").password("{noop}joey123").roles("ADMIN").build();
+//        UserDetails ross = User.withUsername("ross").password("{noop}ross123").roles("MANAGER", "ADMIN").build();
+//        return new InMemoryUserDetailsManager(monica, joey, ross);
+//    }
+
+
     @Bean
     public UserDetailsService userDetailsService(){
-        UserDetails monica = User.withUsername("monica").password("{noop}monica123").roles("MANAGER").build();
-        UserDetails joey = User.withUsername("joey").password("{noop}joey123").roles("ADMIN").build();
-        UserDetails ross = User.withUsername("ross").password("{noop}ross123").roles("MANAGER", "ADMIN").build();
-        return new InMemoryUserDetailsManager(monica, joey, ross);
+        UserDetails monica = User.withUsername("monica").password(passwordEncoder().encode("monica123")).roles("MANAGER").build();
+        UserDetails joey = User.withUsername("joey").password(passwordEncoder().encode("joey123")).roles("ADMIN").build();
+        UserDetails ross = User.withUsername("ross").password(passwordEncoder().encode("ross123")).roles("MANAGER", "ADMIN").build();
+
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+        jdbcUserDetailsManager.createUser(monica);
+        jdbcUserDetailsManager.createUser(joey);
+        jdbcUserDetailsManager.createUser(ross);
+        return jdbcUserDetailsManager;
     }
 
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
--- day04/accounts/src/main/resources/schema.sql
+++ day04/accounts/src/main/resources/schema.sql
@@ -12,3 +12,16 @@ CREATE TABLE IF NOT EXISTS `accounts` (
     `account_type` VARCHAR(50) NOT NULL,
     `branch_address` VARCHAR(100) NOT NULL
 );
+
+create table users(
+    username varchar_ignorecase(50) not null primary key,
+    password varchar_ignorecase(500) not null,
+    enabled boolean not null
+    );
+create table authorities (
+    username varchar_ignorecase(50) not null,
+    authority varchar_ignorecase(50) not null,
+    constraint fk_authorities_users foreign key(username) references users(username)
+    );
+
+create unique index ix_auth_username on authorities (username,authority);
